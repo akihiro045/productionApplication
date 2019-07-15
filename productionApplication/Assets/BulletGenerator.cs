62e21ded08c6c242619fb877aff7746e22aa4c66
akina <g1727045@st.t-kougei.ac.jp>
2019-07-15 23:25:59 +0900

    銃弾と壁の当たり判定まで

---
+++ productionApplication/Assets/BulletGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerator : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Bullet = Instantiate(BulletPrefab) as GameObject;
+            Bullet.GetComponent<BulletController>().Shoot(new Vector3(1000, 0, 0));
+        }
+    }
+}
---
+++ productionApplication/Assets/Scripts/Main/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
