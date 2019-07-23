bc464947c93c6779f961c7c9e4b722d104383838
akina <g1727045@st.t-kougei.ac.jp>
2019-07-23 14:26:24 +0900

    こみっと

--- productionApplication/Assets/Scripts/Main/WallController.cs
+++ productionApplication/Assets/Scripts/Main/WallController.cs
@@ -6,12 +6,6 @@ public class WallController : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        //銃弾の消去はここでやらないほうが楽かも
-        // if (other.gameObject.tag == "Bullet")
-        // {
-        //     Destroy(other.gameObject);
-        // }
-        // else 
         if (other.gameObject.tag == "Floor")
         {
             GetComponent<Rigidbody>().isKinematic = true;
